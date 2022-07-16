8346b56d2353c108663399249c8fa12731227ec5
nathaliacrz <nathalias161@gmail.com>
2022-07-16 15:45:08 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,13 @@
+import NotificationButton from "./components/NotificationButton"
+
 function App() {
   return (
-  <h1>EAEEE PESSUAAAAAAL</h1>
-    )
-  }
+    <>
+      <h1>EAEEE PESSUAAAAAAL</h1>
+      <NotificationButton />
+      <NotificationButton />
+    </>
+  )
+}
 
 export default App
